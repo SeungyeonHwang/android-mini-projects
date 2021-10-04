be0607c93e016d5507003dd6c764c02b39ed6b63
ファン <syhwang.web@gmail.com>
2021-10-05 01:20:00 +0900

    BTS_ImageView

--- BTS/Bts/app/src/main/java/com/hwang/bts/MainActivity.kt
+++ BTS/Bts/app/src/main/java/com/hwang/bts/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hwang.bts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//화면의 로직을 작성
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
