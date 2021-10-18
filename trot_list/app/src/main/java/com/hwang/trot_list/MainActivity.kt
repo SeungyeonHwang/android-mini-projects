0041373c2ede47dbb6c9fd5625b639326bff9213
ファン <syhwang.web@gmail.com>
2021-10-19 01:56:43 +0900

    +FireBase Practice

---
+++ trot_list/app/src/main/java/com/hwang/trot_list/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hwang.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1. Navigation
+
+        //2. Fragement ->  Nav 연결
+            //2.1 Fragment에서 버튼(이미지) 누르면 이동
+
+        //3. 리사이클러 뷰
+    }
+}
