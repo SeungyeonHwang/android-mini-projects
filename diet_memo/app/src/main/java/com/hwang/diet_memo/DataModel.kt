9301114495550a1cffd621c761b65585fe8b319a
ファン <syhwang.web@gmail.com>
2021-10-21 00:41:47 +0900

    #Diet App Done

---
+++ diet_memo/app/src/main/java/com/hwang/diet_memo/DataModel.kt
@@ -0,0 +1,6 @@
+package com.hwang.diet_memo
+
+data class DataModel(
+    val date: String = "",
+    val memo: String = ""
+)
--- diet_memo/app/src/main/java/com/hwang/diet_memo/MainActivity.kt
+++ diet_memo/app/src/main/java/com/hwang/diet_memo/MainActivity.kt
@@ -1,11 +1,107 @@
 package com.hwang.diet_memo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val dataModelList = mutableListOf<DataModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("myMemo")
+
+        val listView = findViewById<ListView>(R.id.mainLV)
+
+        val adapterList = ListViewAdapter(dataModelList)
+
+        listView.adapter = adapterList
+
+        Log.d("DataModel---------", dataModelList.toString())
+
+        myRef.child(Firebase.auth.currentUser!!.uid).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                dataModelList.clear()
+
+                for (dataModel in snapshot.children) {
+                    Log.d("Data", dataModel.toString())
+                    dataModelList.add(dataModel.getValue(DataModel::class.java)!!)
+                }
+
+                adapterList.notifyDataSetChanged()
+                Log.d("DataModel", dataModelList.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        val writeBtn = findViewById<ImageView>(R.id.writeBtn)
+        writeBtn.setOnClickListener {
+
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custome_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
+
+            val mAlertDialog = mBuilder.show()
+
+            val DateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            var dateText = ""
+
+            DateSelectBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year = today.get(Calendar.YEAR)
+                val month = today.get(Calendar.MONTH)
+                val date = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                    override fun onDateSet(view: DatePicker?, year: Int, month: Int, date: Int) {
+                        Log.d("MAIN", "$year, ${month + 1}, $date")
+                        //날짜 데이터
+                        DateSelectBtn.text = "$year, ${month + 1}, $date"
+
+                        dateText = "$year, ${month + 1}, $date"
+                    }
+                }, year, month, date)
+                Log.d("MAIN", "click")
+                dlg.show()
+            }
+
+            val saveBtn = mAlertDialog.findViewById<Button>(R.id.saveBtn)
+            saveBtn?.setOnClickListener {
+
+                //Memo 데이터
+                val healthMemo =
+                    mAlertDialog.findViewById<EditText>(R.id.healthMemo)?.text.toString()
+
+                val database = Firebase.database
+                val myRef = database.getReference("myMemo").child(Firebase.auth.currentUser!!.uid)
+
+                val model = DataModel(dateText, healthMemo)
+                myRef
+                    .push()
+                    .setValue(model)
+                mAlertDialog.dismiss()
+            }
+        }
     }
 }
